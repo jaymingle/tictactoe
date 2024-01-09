8b35133099efb432fe34eadca8c0f3719fbce3b5
Jay Mingle <jkawudi@gmail.com>
2024-01-09 14:23:30 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import {useState} from "react";
 function App() {
 
     const [squares, setSquares] = useState(Array(9).fill(null))
-
+    const handleClick = (i) => {
+        const nextSquares = squares.slice()
+        nextSquares[i] = "X"
+        setSquares(nextSquares)
+    }
   return (
     <>
       <div className="app-class">
           <p>Welcome to Ghana</p>
           <div className="board-row">
-              <Square value={squares[0]} onSquareClick={handleClick}/>
-              <Square value={squares[1]} onSquareClick={handleClick}/>
-              <Square value={squares[2]} onSquareClick={handleClick}/>
+              <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+              <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+              <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
           </div>
 
           <div className="board-row">
