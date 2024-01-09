3b521fbb30a9a06f1dce7d907f730ce98ede1d79
Jay Mingle <jkawudi@gmail.com>
2024-01-09 10:33:42 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,24 @@ function App() {
     <>
       <div className="app-class">
           <p>Welcome to Ghana</p>
-          <button className="square">X</button>
-          <button className="square">X</button>
-          <button className="square">X</button>
-
           <div className="board-row">
-
+              <button className="square">X</button>
+              <button className="square">X</button>
+              <button className="square">X</button>
           </div>
 
           <div className="board-row">
-
+              <button className="square">X</button>
+              <button className="square">X</button>
+              <button className="square">X</button>
           </div>
 
           <div className="board-row">
-
+              <button className="square">X</button>
+              <button className="square">X</button>
+              <button className="square">X</button>
           </div>
 
-          <button className="square">X</button>
-          <button className="square">X</button>
-          <button className="square">X</button>
-          <button className="square">X</button>
-          <button className="square">X</button>
-          <button className="square">X</button>
       </div>
     </>
   )
