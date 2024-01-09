30f034d7b078e4a23573a477bc88fc09274e8bfc
Jay Mingle <jkawudi@gmail.com>
2024-01-09 12:59:54 +0000

    Changes

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,6 @@
-import {useState} from "react";
 
 const Square = ({value}) => {
 
-    // const [value, setValue] = useState(null)
-
-    const squareClickHandler = () => {
-        // console.log('Clicked')
-        setValue("X")
-    }
 
     return (
         <button className="square" onClick={squareClickHandler}>{value}</button>
